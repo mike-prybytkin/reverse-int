198546372f810ebf2a3387dfa293845f5d0519bf
@Mike-Prybytkin <prybytkin@bk.ru>
2022-01-29 17:10:41 +0300

    feat: function reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let num = Math.abs(n);
+    let array = (""+num).split("").map(Number).reverse();
+    let reverseNum = '';
+    array.forEach(el => reverseNum += `${el}`);
+
+    return Number(reverseNum);
 }
